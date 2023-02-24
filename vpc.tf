d3be4e24dbd484dce85fe06f2e51139ffbce2ff7
Leosapucahy <126193875+Leeosapucahy@users.noreply.github.com>
2023-02-24 17:52:48 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,19 +3,19 @@ data "aws_vpc" "NaoProd" {
 }
 
 resource "aws_subnet" "privatesubnet-sin-itau-1a"{
-  vpc_id = ""
+  vpc_id = "coloque aqui sua VPC"
   cidr_block              = cidrsubnet(data.aws_vpc.NaoProd.cidr_block, 8, 0) ## takes 10.0.0.0/16 --> 10.0.1.0/24
   map_public_ip_on_launch = true
   tags = {
-    Name = "privatesubnet-sin-itau-1a"
+    Name = "Coloque aqui o nome da Subnet"
   }
 }
 
 resource "aws_subnet" "privatesubnet-sin-itau-1b"{
-  vpc_id = ""
+  vpc_id = "coloque aqui sua VPC"
   cidr_block              = cidrsubnet(data.aws_vpc.NaoProd.cidr_block, 8, 5) ## takes 10.0.0.0/16 --> 10.0.1.0/24
   map_public_ip_on_launch = true
   tags = {
-    Name = "privatesubnet-sin-itau-1b"
+    Name = "coloque aqui o nome da Subnet"
   }
 }
